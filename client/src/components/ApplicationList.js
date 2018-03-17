14e46b147a110d6e04e7571f6d6852de24e2f97a
quietmind <adammateo@gmail.com>
2018-03-17 17:27:13 -0400

    /feat Main App Container scaffolding

---
+++ client/src/components/ApplicationList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default class AppList extends React.Component{
+	constructor(props){
+		super(props)
+		this.state ={
+
+		}
+	}
+
+	render(){
+		return(
+			<div> This is the Application List </div>
+		)
+	}
+}
---
+++ client/src/components/ProgressBoard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default class ProgressBoard extends React.Component{
+	constructor(props){
+		super(props)
+		this.state ={
+
+		}
+	}
+
+	render(){
+		return(
+			<div> This is the progress board </div>
+		)
+	}
+}
