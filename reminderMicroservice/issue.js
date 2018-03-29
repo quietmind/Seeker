354c09bd30d3f789d6024ae8b414af6464dcb789
quietmind <adammateo@gmail.com>
2018-03-29 15:22:27 -0400

    refactor notifications

--- reminderMicroservice/issue.js
+++ reminderMicroservice/issue.js
@@ -1,15 +1,17 @@
 const axios = require('axios')
 
-const sendReminder = (reminder) =>{
-  var rootUrl = 'https://api.elasticemail.com/v2/email/send?apikey=11247b43-8015-4e70-b075-4327381d0e0f'
-  var subject = `&subject="HEY! DON'T FORGET TO..."`
-  var sender = '&from=' + 'adammateo@gmail.com'
-  var senderName = '&fromName' + 'Seeker'
-  var receiver = '&to=' + `adammateo@gmail.com` //donaters email address
-  var message = '&bodyText=' + 'Hey there, you have asked us to remind you about ' +  reminder.text_desc + '. \n\n Thanks for using Seeker! \n\n'
-  var isTransactional = '&isTransactional=true'
+const sendReminder = (packet) =>{
+  let user = packet.user
+  let reminder = packet.reminder
+  let rootUrl = 'https://api.elasticemail.com/v2/email/send?apikey=11247b43-8015-4e70-b075-4327381d0e0f'
+  let subject = `&subject="HEY! DON'T FORGET TO..."`
+  let sender = '&from=' + 'adammateo@gmail.com'
+  let senderName = '&fromName' + 'Seeker'
+  let receiver = '&to=' + `${user}` //donaters email address
+  let message = '&bodyText=' + 'Hey there, you have asked us to remind you about ' + reminder + '. \n\n Thanks for using Seeker! \n\n'
+  let isTransactional = '&isTransactional=true'
 
-  var URL = rootUrl + subject + sender + senderName + receiver + message + isTransactional
+  let URL = rootUrl + subject + sender + senderName + receiver + message + isTransactional
 
   axios.post(URL)
   .then((response) => {
