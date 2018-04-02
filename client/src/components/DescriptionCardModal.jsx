9e98d0ef17b5a578d1b60f770e07263a26a0c770
Admin <admin@Admins-MacBook-Air.local>
2018-04-02 16:48:28 -0400

    Renders. Does it work?

--- client/src/components/DescriptionCardModal.jsx
+++ client/src/components/DescriptionCardModal.jsx
@@ -12,8 +12,6 @@ class DescriptionCard extends React.Component{
     super(props)
 
     this.state = {
-      date: moment(),
-      reminderText: '',
       open: false,
       notesText: '',
       notes: [],
@@ -23,16 +21,11 @@ class DescriptionCard extends React.Component{
       contactEmail: '',
       title: '',
       department: '',
-      reminderId: null,
       contactId: null,
       activeItem: 'Recap'
     }
 
-    this.addContact               = this.addContact.bind(this)
-    this.sendReminder             = this.sendReminder.bind(this);
-    this.handleChange             = this.handleChange.bind(this);
     this.deleteApplication        = this.deleteApplication.bind(this);
-    this.addNote                  = this.addNote.bind(this);
     this.handleClose              = this.handleClose.bind(this);
     this.handleOpen               = this.handleOpen.bind(this);
     this.askPermission            = this.askPermission.bind(this);
@@ -40,9 +33,6 @@ class DescriptionCard extends React.Component{
     this.subscribeUser            = this.subscribeUser.bind(this);
     this.sendSubscriptionToServer = this.sendSubscriptionToServer.bind(this);
     this.handleItemClick          = this.handleItemClick.bind(this);
-    this.decorateRecap            = this.decorateRecap.bind(this);
-    this.decorateNotes            = this.decorateNotes.bind(this);
-    this.decorateReminders        = this.decorateReminders.bind(this);
   }
 
   componentWillMount(){
@@ -51,88 +41,17 @@ class DescriptionCard extends React.Component{
 
   handleOpen() {
     this.props.keepSwitch();
-    this.setState({ open: true }, this.props.toggle)
+    this.setState({ open: true }, this.props.toggle);
   }
 
   handleClose() {
     this.setState({ reminderText: '', notesText: '', open: false });
   }
 
-  handleChange(date) {
-    this.setState({
-      date: date
-    })
-  }
-
-  sendReminder() {
-    if (this.props.email) {
-      axios.post('/reminders', {
-        date: this.state.date,
-        description: this.state.reminderText,
-        email: this.props.email,
-        company: this.props.app.company,
-        job_title: this.props.app.job_title,
-        userId: this.props.userId,
-        point_of_contact: this.props.app.point_of_contact,
-        appId: this.props.app.id
-      })
-      .then((response) => this.setState({reminderId: response.data}))
-      .catch((err) => console.error(err))
-    } else {
-      axios.post('/calendar', {
-        date: `${this.state.date._d.getFullYear()}-${this.state.date._d.getMonth()+1}-${this.state.date._d.getDate()}`,
-        description: this.state.reminderText,
-        email: this.props.email,
-        company: this.props.app.company,
-        job_title: this.props.app.job_title,
-        userId: this.props.userId,
-        point_of_contact: this.props.app.point_of_contact,
-        appId: this.props.app.id
-      })
-      .then((response) => this.setState({reminderId: response.data}))
-      .catch((err) => console.error(err))
-    }
-  }
-
-  addContact() {
-    if (this.props.email) {
-      axios.post('/contacts', {
-        firstName: this.state.firstName,
-        lastName: this.state.lastName,
-        contactPhone: this.state.contactPhone,
-        contactEmail: this.state.contactEmail,
-        company: this.props.app.company,
-        title: this.state.title,
-        department: this.state.department
-      })
-      .then((response) => this.setState({contactId: response.data}))
-      .catch((err) => console.error(err))
-    } else {
-      axios.post('/people', {
-        firstName: this.state.firstName,
-        lastName: this.state.lastName,
-        contactPhone: this.state.contactPhone,
-        contactEmail: this.state.contactEmail,
-        company: this.props.app.company,
-        title: this.state.title,
-        department: this.state.department
-      })
-      .then((response) => this.setState({contactId: response.data}))
-      .catch((err) => console.error(err))
-    }
-  }
-
   deleteApplication() {
     axios.delete('/applications', {data: {appId: this.props.app.id}})
     .then((response) => this.props.handleClick())
     .then((response) => this.handleClose())
-    .catch((err) => console.error(err))
-  }
-
-  addNote() {
-    axios.post('/notes', {appId: this.props.app.id, text: this.state.notesText, userId: this.props.id})
-    .then(() => this.props.handleClick())
-    .then(() => this.setState({notesText: ''}))
     .catch((err) => console.error(err))
   }
 
@@ -219,69 +138,19 @@ class DescriptionCard extends React.Component{
     this.setState({ activeItem: name });
   }
 
-  decorateRecap() {
-    return (
-      <div>
-        <p>Resume Provided:</p>
-        <a href={this.props.resume ? this.props.resume.s3_url :  ''}>{this.props.resume ? this.props.resume.file_name : ''}</a>
-        <p>Cover Letter Provided:</p>
-        <a href={this.props.coverletter ? this.props.coverletter.s3_url :  ''}>{this.props.coverletter ? this.props.coverletter.file_name : ''}</a>
-        <p>Created At:<br></br>
-        {new Date(this.props.app.date_created).toDateString().substring(4)}</p>
-        <p>Last Activity:<br></br>
-        {new Date(this.props.app.last_update).toDateString().substring(4)}</p>
-      </div>
-    )
-  }
-
-  decorateNotes() {
-    return (
-      <div>
-        <p>Add a note to this entry</p>
-        <input type="text" value={this.state.notesText} placeholder="Notes Description" onChange={(e) => this.setState({notesText: e.target.value})}></input>
-        <button onClick={this.addNote}>Submit</button>
-      <h1>Notes:</h1>
-      {this.props.notes.map((note, i) => (
-        <p key={i}>{note.note_text}</p>
-      ))}
-      </div>
-    )
-  }
-
-  decorateReminders() {
-    return (
-      <div className="reminder">
-        <p>Add a Reminder</p>
-        <DatePicker
-          selected={this.state.date}
-          onChange={this.handleChange}
-          placeholderText="Choose a date"
-          dateFormat="LLL"
-        />
-        <input type="text" value={this.state.reminderText} placeholder="Reminder Description" onChange={(e) => this.setState({reminderText: e.target.value})}></input>
-        <button onClick={this.sendReminder}>Submit</button>
-      </div>
-    )
-  }
-
-  decorateContact() {
-    return (
-      <div className="contact">
-        <p>Add a Point of Contact</p>
-        <input type="text" value={this.state.firstName} placeholder="First Name" onChange={(event) => this.setState({firstName: event.target.value})}></input>
-        <input type="text" value={this.state.lastName} placeholder="Last Name" onChange={(event) => this.setState({lastName: event.target.value})}></input>
-        <input type="text" value={this.state.contactPhone} placeholder="Phone #" onChange={(event) => this.setState({contactPhone: event.target.value})}></input>
-        <input type="text" value={this.state.contactEmail} placeholder="Email" onChange={(event) => this.setState({contactEmail: event.target.value})}></input>
-        <input type="text" value={this.state.title} placeholder="Title" onChange={(event) => this.setState({title: event.target.value})}></input>
-        <input type="text" value={this.state.department} placeholder="Department" onChange={(event) => this.setState({department: event.target.value})}></input>
-        <button onClick={this.addContact}>Submit</button>
-      </div>
-    )
-
-  }
-
   render() {
     const { activeItem } = this.state;
+    var cardContent;
+
+    if (activeItem === 'Recap') {
+      cardContent = <Recap resume={this.props.resume} coverletter={this.props.coverletter} app={this.props.app} />;
+    } else if (activeItem === 'Notes') {
+      cardContent = <Notes notes={this.props.notes} />;
+    } else if (activeItem === 'Reminder') {
+      cardContent = <Reminder email={this.props.email} app={this.props.app} />;
+    } else if (activeItem === 'Contact') {
+      cardContent = <Contact app={this.props.app}/>;
+    }
 
     return (
       <Modal
@@ -313,15 +182,181 @@ class DescriptionCard extends React.Component{
           <Menu.Item name='Contact' active={activeItem === 'Contact'}
             as={Link} to='/list/contact' onClick={this.handleItemClick} />
         </Menu>
-        <Switch>
-          <Route  exact path='/list/recap' render={this.decorateRecap}/>
-          <Route  path='/list/notes' render={this.decorateNotes}/>
-          <Route  path='/list/reminder' render={this.decorateReminder}/>
-          <Route path='/list/contact' render={this.decorateContact}/>
-        </Switch>
+        {cardContent}
         <button onClick={this.deleteApplication}>Delete this Application</button>
       </Modal.Content>
     </Modal>
+    )
+  }
+}
+
+const Recap = (props) => (
+  <div>
+    <p>Resume Provided:</p>
+    <a href={props.resume ? props.resume.s3_url :  ''}>{props.resume ? props.resume.file_name : ''}</a>
+    <p>Cover Letter Provided:</p>
+    <a href={props.coverletter ? props.coverletter.s3_url :  ''}>{props.coverletter ? props.coverletter.file_name : ''}</a>
+    <p>Created At:<br></br>
+    {new Date(props.app.date_created).toDateString().substring(4)}</p>
+    <p>Last Activity:<br></br>
+    {new Date(props.app.last_update).toDateString().substring(4)}</p>
+  </div>
+)
+
+class Notes extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+        notesText: ''
+    }
+    this.addNote = this.addNote.bind(this);
+  }
+
+  addNote() {
+    axios.post('/notes', {appId: this.props.app.id, text: this.state.notesText, userId: this.props.id})
+    .then(() => this.props.handleClick())
+    .then(() => this.setState({notesText: ''}))
+    .catch((err) => console.error(err))
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Add a note to this entry</p>
+        <input type="text" value={this.state.notesText} placeholder="Notes Description" onChange={(e) => this.setState({notesText: e.target.value})}></input>
+        <button onClick={this.addNote}>Submit</button>
+      <h1>Notes:</h1>
+      {this.props.notes.map((note, i) => (
+        <p key={i}>{note.note_text}</p>
+      ))}
+      </div>
+    )
+  }
+}
+
+class Reminder extends React.Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      date: moment(),
+      reminderText: '',
+      reminderId: null,
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.sendReminder = this.sendReminder.bind(this);
+  }
+
+  handleChange(date) {
+    this.setState({
+      date: date
+    })
+  }
+
+  sendReminder() {
+    if (this.props.email) {
+      axios.post('/reminders', {
+        date: this.state.date,
+        description: this.state.reminderText,
+        email: this.props.email,
+        company: this.props.app.company,
+        job_title: this.props.app.job_title,
+        userId: this.props.app.userId,
+        point_of_contact: this.props.app.point_of_contact,
+        appId: this.props.app.id
+      })
+      .then((response) => this.setState({reminderId: response.data}))
+      .catch((err) => console.error(err))
+    } else {
+      axios.post('/calendar', {
+        date: `${this.state.date._d.getFullYear()}-${this.state.date._d.getMonth()+1}-${this.state.date._d.getDate()}`,
+        description: this.state.reminderText,
+        email: this.props.email,
+        company: this.props.app.company,
+        job_title: this.props.app.job_title,
+        userId: this.props.userId,
+        point_of_contact: this.props.app.point_of_contact,
+        appId: this.props.app.id
+      })
+      .then((response) => this.setState({reminderId: response.data}))
+      .catch((err) => console.error(err))
+    }
+  }
+
+  render() {
+    return (
+      <div className="reminder">
+        <p>Add a Reminder</p>
+        <DatePicker
+          selected={this.state.date}
+          onChange={this.handleChange}
+          placeholderText="Choose a date"
+          dateFormat="LLL"
+        />
+        <input type="text" value={this.state.reminderText} placeholder="Reminder Description" onChange={(e) => this.setState({reminderText: e.target.value})}></input>
+        <button onClick={this.sendReminder}>Submit</button>
+      </div>
+    )
+  }
+}
+
+class Contact extends React.Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      firstName: '',
+      lastName: '',
+      contactPhone: '',
+      contactEmail: '',
+      title: '',
+      department: ''
+    }
+
+    this.addContact = this.addContact.bind(this);
+  }
+
+  addContact() {
+    if (this.props.email) {
+      axios.post('/contacts', {
+        firstName: this.state.firstName,
+        lastName: this.state.lastName,
+        contactPhone: this.state.contactPhone,
+        contactEmail: this.state.contactEmail,
+        company: this.props.app.company,
+        title: this.state.title,
+        department: this.state.department
+      })
+      .then((response) => this.setState({contactId: response.data}))
+      .catch((err) => console.error(err))
+    } else {
+      axios.post('/people', {
+        firstName: this.state.firstName,
+        lastName: this.state.lastName,
+        contactPhone: this.state.contactPhone,
+        contactEmail: this.state.contactEmail,
+        company: this.props.app.company,
+        title: this.state.title,
+        department: this.state.department
+      })
+      .then((response) => this.setState({contactId: response.data}))
+      .catch((err) => console.error(err))
+    }
+  }
+
+  render() {
+    return (
+      <div className="contact">
+        <p>Add a Point of Contact</p>
+        <input type="text" value={this.state.firstName} placeholder="First Name" onChange={(event) => this.setState({firstName: event.target.value})}></input>
+        <input type="text" value={this.state.lastName} placeholder="Last Name" onChange={(event) => this.setState({lastName: event.target.value})}></input>
+        <input type="text" value={this.state.contactPhone} placeholder="Phone #" onChange={(event) => this.setState({contactPhone: event.target.value})}></input>
+        <input type="text" value={this.state.contactEmail} placeholder="Email" onChange={(event) => this.setState({contactEmail: event.target.value})}></input>
+        <input type="text" value={this.state.title} placeholder="Title" onChange={(event) => this.setState({title: event.target.value})}></input>
+        <input type="text" value={this.state.department} placeholder="Department" onChange={(event) => this.setState({department: event.target.value})}></input>
+        <button onClick={this.addContact}>Submit</button>
+      </div>
     )
   }
 }
--- client/src/components/Phase.jsx
+++ client/src/components/Phase.jsx
@@ -20,29 +20,29 @@ export default class Phase extends Component {
 		return(
 			<div className='phase' id={this.props.phase.id}>
 			<div className='PhaseTitle' id="title">
-			<Header 
-			  className="phasetitle" 
-			  textAlign="center" 
-			  block 
-			  inverted 
+			<Header
+			  className="phasetitle"
+			  textAlign="center"
+			  block
+			  inverted
 			  size="large">{this.props.phase.phase_label}
-				<Icon 
-				  className="headericon" 
+				<Icon
+				  className="headericon"
 				  name="ellipsis vertical"
 				  textAlign="right"
 				  onClick={this.clickHandler}/>
 				  <Header.Subheader>
 				  	{this.props.applications.length} Jobs
 				  </Header.Subheader>
-				  </Header> 
+				  </Header>
 			</div>
 			{
 		      this.props.applications.map((app,i) =>  {
 		      	return <div id={app.id} key={i}>
-		      	<Card 
-		      	  className="AppItem" 
-		      	  header={app.company} 
-		      	  description={app.job_title} 
+		      	<Card
+		      	  className="AppItem"
+		      	  header={app.company}
+		      	  description={app.job_title}
 		      	  meta={`Last Update: ${moment(app.last_update).format('MM/DD')}`}/>
 		      	  </div>
 		      })
