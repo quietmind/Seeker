e2c1286bb3df007bcdd8ce255b4feaae67174dd2
Admin <admin@Admins-MacBook-Air.local>
2018-04-02 11:52:38 -0400

    trouble displaying notes

--- client/src/components/DescriptionCardModal.jsx
+++ client/src/components/DescriptionCardModal.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Button, Header, Icon, Modal, Table } from 'semantic-ui-react';
+import { Button, Header, Icon, Modal, Table, Menu } from 'semantic-ui-react';
 import DatePicker from 'react-datepicker';
 import moment from 'moment';
 import axios from 'axios';
+import { BrowserRouter as Router, Route, Link, Switch, withRouter } from 'react-router-dom';
 
 let pubKey = 'BKn9Z71eyV2fgYztoT3XDC31ANF3HLmKuXKmkQR9OoMw-9trIi4JguYx-Y5kJ0xLddXlJTrPWmpnWcA5ebFHRfY';
 
@@ -23,7 +24,8 @@ class DescriptionCard extends React.Component{
       title: '',
       department: '',
       reminderId: null,
-      contactId: null
+      contactId: null,
+      activeItem: 'Recap'
     }
 
     this.addContact               = this.addContact.bind(this)
@@ -34,9 +36,13 @@ class DescriptionCard extends React.Component{
     this.handleClose              = this.handleClose.bind(this);
     this.handleOpen               = this.handleOpen.bind(this);
     this.askPermission            = this.askPermission.bind(this);
-    this.urlB64ToUint8Array       = this.urlB64ToUint8Array.bind(this)
-    this.subscribeUser            = this.subscribeUser.bind(this)
-    this.sendSubscriptionToServer = this.sendSubscriptionToServer.bind(this)
+    this.urlB64ToUint8Array       = this.urlB64ToUint8Array.bind(this);
+    this.subscribeUser            = this.subscribeUser.bind(this);
+    this.sendSubscriptionToServer = this.sendSubscriptionToServer.bind(this);
+    this.handleItemClick          = this.handleItemClick.bind(this);
+    this.decorateRecap            = this.decorateRecap.bind(this);
+    this.decorateNotes            = this.decorateNotes.bind(this);
+    this.decorateReminders        = this.decorateReminders.bind(this);
   }
 
   componentWillMount(){
@@ -209,8 +215,73 @@ class DescriptionCard extends React.Component{
   });
 }
 
+  handleItemClick(e, { name }) {
+    this.setState({ activeItem: name });
+  }
+
+  decorateRecap() {
+    return (
+      <div>
+        <p>Resume Provided:</p>
+        <a href={this.props.resume ? this.props.resume.s3_url :  ''}>{this.props.resume ? this.props.resume.file_name : ''}</a>
+        <p>Cover Letter Provided:</p>
+        <a href={this.props.coverletter ? this.props.coverletter.s3_url :  ''}>{this.props.coverletter ? this.props.coverletter.file_name : ''}</a>
+        <p>Created At:<br></br>
+        {new Date(this.props.app.date_created).toDateString().substring(4)}</p>
+        <p>Last Activity:<br></br>
+        {new Date(this.props.app.last_update).toDateString().substring(4)}</p>
+      </div>
+    )
+  }
+
+  decorateNotes() {
+    return (
+      <div>
+        <p>Add a note to this entry</p>
+        <input type="text" value={this.state.notesText} placeholder="Notes Description" onChange={(e) => this.setState({notesText: e.target.value})}></input>
+        <button onClick={this.addNote}>Submit</button>
+      <h1>Notes:</h1>
+      {this.props.notes.map((note, i) => (
+        <p key={i}>{note.note_text}</p>
+      ))}
+      </div>
+    )
+  }
+
+  decorateReminders() {
+    return (
+      <div className="reminder">
+        <p>Add a Reminder</p>
+        <DatePicker
+          selected={this.state.date}
+          onChange={this.handleChange}
+          placeholderText="Choose a date"
+          dateFormat="LLL"
+        />
+        <input type="text" value={this.state.reminderText} placeholder="Reminder Description" onChange={(e) => this.setState({reminderText: e.target.value})}></input>
+        <button onClick={this.sendReminder}>Submit</button>
+      </div>
+    )
+  }
+
+  decorateContact() {
+    return (
+      <div className="contact">
+        <p>Add a Point of Contact</p>
+        <input type="text" value={this.state.firstName} placeholder="First Name" onChange={(event) => this.setState({firstName: event.target.value})}></input>
+        <input type="text" value={this.state.lastName} placeholder="Last Name" onChange={(event) => this.setState({lastName: event.target.value})}></input>
+        <input type="text" value={this.state.contactPhone} placeholder="Phone #" onChange={(event) => this.setState({contactPhone: event.target.value})}></input>
+        <input type="text" value={this.state.contactEmail} placeholder="Email" onChange={(event) => this.setState({contactEmail: event.target.value})}></input>
+        <input type="text" value={this.state.title} placeholder="Title" onChange={(event) => this.setState({title: event.target.value})}></input>
+        <input type="text" value={this.state.department} placeholder="Department" onChange={(event) => this.setState({department: event.target.value})}></input>
+        <button onClick={this.addContact}>Submit</button>
+      </div>
+    )
+
+  }
 
   render() {
+    const { activeItem } = this.state;
 
     return (
       <Modal
@@ -230,50 +301,25 @@ class DescriptionCard extends React.Component{
       closeIcon={true}
       size='small'
       >
-      <Header icon='building' content={this.props.app.company} />
+      <Header icon='building' content={this.props.app.job_title + " at " + this.props.app.company} />
       <Modal.Content>
-        <h1>{this.props.app.job_title}</h1>
-        <p>Resume Provided:</p>
-        <a href={this.props.resume ? this.props.resume.s3_url :  ''}>{this.props.resume ? this.props.resume.file_name : ''}</a>
-        <p>Cover Letter Provided:</p>
-        <a href={this.props.coverletter ? this.props.coverletter.s3_url :  ''}>{this.props.coverletter ? this.props.coverletter.file_name : ''}</a>
-        <p>Created At:<br></br>
-        {new Date(this.props.app.date_created).toDateString().substring(4)}</p>
-        <p>Last Activity:<br></br>
-        {new Date(this.props.app.last_update).toDateString().substring(4)}</p>
-        <div className="field">
-          <div className="reminder">
-            <p>Add a Reminder</p>
-            <DatePicker
-              selected={this.state.date}
-              onChange={this.handleChange}
-              placeholderText="Choose a date"
-              dateFormat="LLL"
-            />
-            <input type="text" value={this.state.reminderText} placeholder="Reminder Description" onChange={(event) => this.setState({reminderText: event.target.value})}></input>
-            <button onClick={this.sendReminder}>Submit</button>
-          </div>
-          <div className="contact">
-            <p>Add a Point of Contact</p>
-            <input type="text" value={this.state.firstName} placeholder="First Name" onChange={(event) => this.setState({firstName: event.target.value})}></input>
-            <input type="text" value={this.state.lastName} placeholder="Last Name" onChange={(event) => this.setState({lastName: event.target.value})}></input>
-            <input type="text" value={this.state.contactPhone} placeholder="Phone #" onChange={(event) => this.setState({contactPhone: event.target.value})}></input>
-            <input type="text" value={this.state.contactEmail} placeholder="Email" onChange={(event) => this.setState({contactEmail: event.target.value})}></input>
-            <input type="text" value={this.state.title} placeholder="Title" onChange={(event) => this.setState({title: event.target.value})}></input>
-            <input type="text" value={this.state.department} placeholder="Department" onChange={(event) => this.setState({department: event.target.value})}></input>
-            <button onClick={this.addContact}>Submit</button>
-          </div>
-          <div>
-            <p>Add a note to this entry</p>
-            <input type="text" value={this.state.notesText} placeholder="Notes Description" onChange={(event) => this.setState({notesText: event.target.value})}></input>
-            <button onClick={this.addNote}>Submit</button>
-          </div>
-          <h1>Notes:</h1>
-          {this.props.notes.map((note, i) => (
-            <p key={i}>{note.note_text}</p>
-          ))}
-          <button onClick={this.deleteApplication}>Delete this Application</button>
-        </div>
+        <Menu tabular>
+          <Menu.Item name='Recap' active={activeItem === 'Recap'}
+            as={Link} to='/list/recap' onClick={this.handleItemClick} />
+          <Menu.Item name='Notes' active={activeItem === 'Notes'}
+            as={Link} to='/list/notes' onClick={this.handleItemClick} />
+          <Menu.Item name='Reminder' active={activeItem === 'Reminder'}
+            as={Link} to='/list/reminder' onClick={this.handleItemClick} />
+          <Menu.Item name='Contact' active={activeItem === 'Contact'}
+            as={Link} to='/list/contact' onClick={this.handleItemClick} />
+        </Menu>
+        <Switch>
+          <Route  exact path='/list/recap' render={this.decorateRecap}/>
+          <Route  path='/list/notes' render={this.decorateNotes}/>
+          <Route  path='/list/reminder' render={this.decorateReminder}/>
+          <Route path='/list/contact' render={this.decorateContact}/>
+        </Switch>
+        <button onClick={this.deleteApplication}>Delete this Application</button>
       </Modal.Content>
     </Modal>
     )
