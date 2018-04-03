b286ae15989402d1907a91eed8617aaaf9ec3bc3
Marcus Hausler <marcus.hausler@gmail.com>
2018-04-03 13:48:44 -0400

    Move reminder functions to relevant component

--- client/src/components/details modal/DetailsModal.jsx
+++ client/src/components/details modal/DetailsModal.jsx
@@ -18,18 +18,10 @@ class DescriptionCard extends React.Component{
       open: false
     }
 
-    this.deleteApplication        = this.deleteApplication.bind(this);
-    this.handleClose              = this.handleClose.bind(this);
-    this.handleOpen               = this.handleOpen.bind(this);
-    this.askPermission            = this.askPermission.bind(this);
-    this.urlB64ToUint8Array       = this.urlB64ToUint8Array.bind(this);
-    this.subscribeUser            = this.subscribeUser.bind(this);
-    this.sendSubscriptionToServer = this.sendSubscriptionToServer.bind(this);
-    this.handleItemClick          = this.handleItemClick.bind(this);
-  }
-
-  componentWillMount(){
-    if(Notification.permission === 'default') this.askPermission()
+    this.deleteApplication = this.deleteApplication.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+    this.handleOpen = this.handleOpen.bind(this);
+    this.handleItemClick = this.handleItemClick.bind(this);
   }
 
   handleOpen() {
@@ -40,94 +32,16 @@ class DescriptionCard extends React.Component{
   handleClose() {
     this.setState({ notesText: '', open: false });
   }
+  
+  handleItemClick(e, { name }) {
+    this.setState({ activeItem: name });
+  }
 
   deleteApplication() {
     axios.delete('/applications', {data: {appId: this.props.app.id}})
     .then((response) => this.props.handleClick())
     .then((response) => this.handleClose())
     .catch((err) => console.error(err))
-  }
-
-  urlB64ToUint8Array(base64String) {
-    const padding = '='.repeat((4 - base64String.length % 4) % 4);
-    const base64 = (base64String + padding)
-      .replace(/\-/g, '+')
-      .replace(/_/g, '/');
-
-    const rawData = window.atob(base64);
-    const outputArray = new Uint8Array(rawData.length);
-
-    for (let i = 0; i < rawData.length; ++i) {
-      outputArray[i] = rawData.charCodeAt(i);
-    }
-    return outputArray;
-  }
-
-  subscribeUser() {
-   navigator.serviceWorker.ready.then((reg) => {
-    let subscribeParams = { userVisibleOnly: true };
-    //Setting the public key of our VAPID key pair.
-    let applicationServerKey = this.urlB64ToUint8Array(pubKey);
-    subscribeParams.applicationServerKey = applicationServerKey;
-    reg.pushManager.subscribe(subscribeParams)
-        .then((subscription) => {
-          console.log(JSON.stringify(subscription))
-            // Update status to subscribe current user on server, and to let
-            // other users know this user has subscribed
-            let endpoint = subscription.endpoint;
-            let key = subscription.getKey('p256dh');
-            let auth = subscription.getKey('auth');
-            let encodedKey = btoa(String.fromCharCode.apply(null, new Uint8Array(key)));
-            let encodedAuth = btoa(String.fromCharCode.apply(null, new Uint8Array(auth)));
-
-            const subscripObject = {
-              endpoint: endpoint,
-              keys: {
-                "p256dh": encodedKey,
-                "auth": encodedAuth
-              }
-
-            }
-            this.sendSubscriptionToServer(subscripObject)
-        })
-        .catch((err) => {
-          // A problem occurred with the subscription.
-          console.log('Unable to subscribe to push.', err);
-        });
-    });
-
-  }
-
-  sendSubscriptionToServer(subscription) {
-    console.log('sending subscription')
-
-    axios.post('/saveSubscription', {
-      data: JSON.stringify(subscription)
-    })
-    .then((res) => console.log(JSON.stringify(res)))
-    .catch((err) => console.error(err))
-  }
-
-  askPermission() {
-  return new Promise( (resolve, reject) => {
-    const permissionResult = Notification.requestPermission(function(result) {
-      resolve(result);
-    });
-
-    if (permissionResult) permissionResult.then(resolve, reject);
-
-  })
-  .then( (permissionResult) => {
-    if (permissionResult !== 'granted') {
-      throw new Error('We weren\'t granted permission.');
-    } else {
-      this.subscribeUser()
-    }
-  });
-}
-
-  handleItemClick(e, { name }) {
-    this.setState({ activeItem: name });
   }
 
   render() {
--- client/src/components/details modal/Reminder.jsx
+++ client/src/components/details modal/Reminder.jsx
@@ -13,6 +13,14 @@ class Reminder extends React.Component {
     }
     this.handleChange = this.handleChange.bind(this);
     this.sendReminder = this.sendReminder.bind(this);
+    this.askPermission = this.askPermission.bind(this);
+    this.urlB64ToUint8Array = this.urlB64ToUint8Array.bind(this);
+    this.subscribeUser = this.subscribeUser.bind(this);
+    this.sendSubscriptionToServer = this.sendSubscriptionToServer.bind(this);
+  }
+
+  componentWillMount(){
+    if(Notification.permission === 'default') this.askPermission()
   }
 
   handleChange(date) {
@@ -20,6 +28,84 @@ class Reminder extends React.Component {
       date: date
     })
   }
+
+  urlB64ToUint8Array(base64String) {
+    const padding = '='.repeat((4 - base64String.length % 4) % 4);
+    const base64 = (base64String + padding)
+      .replace(/\-/g, '+')
+      .replace(/_/g, '/');
+
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+
+    for (let i = 0; i < rawData.length; ++i) {
+      outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+  }
+
+  subscribeUser() {
+   navigator.serviceWorker.ready.then((reg) => {
+    let subscribeParams = { userVisibleOnly: true };
+    //Setting the public key of our VAPID key pair.
+    let applicationServerKey = this.urlB64ToUint8Array(pubKey);
+    subscribeParams.applicationServerKey = applicationServerKey;
+    reg.pushManager.subscribe(subscribeParams)
+        .then((subscription) => {
+          console.log(JSON.stringify(subscription))
+            // Update status to subscribe current user on server, and to let
+            // other users know this user has subscribed
+            let endpoint = subscription.endpoint;
+            let key = subscription.getKey('p256dh');
+            let auth = subscription.getKey('auth');
+            let encodedKey = btoa(String.fromCharCode.apply(null, new Uint8Array(key)));
+            let encodedAuth = btoa(String.fromCharCode.apply(null, new Uint8Array(auth)));
+
+            const subscripObject = {
+              endpoint: endpoint,
+              keys: {
+                "p256dh": encodedKey,
+                "auth": encodedAuth
+              }
+
+            }
+            this.sendSubscriptionToServer(subscripObject)
+        })
+        .catch((err) => {
+          // A problem occurred with the subscription.
+          console.log('Unable to subscribe to push.', err);
+        });
+    });
+
+  }
+
+  sendSubscriptionToServer(subscription) {
+    console.log('sending subscription')
+
+    axios.post('/saveSubscription', {
+      data: JSON.stringify(subscription)
+    })
+    .then((res) => console.log(JSON.stringify(res)))
+    .catch((err) => console.error(err))
+  }
+
+  askPermission() {
+  return new Promise( (resolve, reject) => {
+    const permissionResult = Notification.requestPermission(function(result) {
+      resolve(result);
+    });
+
+    if (permissionResult) permissionResult.then(resolve, reject);
+
+  })
+  .then( (permissionResult) => {
+    if (permissionResult !== 'granted') {
+      throw new Error('We weren\'t granted permission.');
+    } else {
+      this.subscribeUser()
+    }
+  });
+}
 
   sendReminder() {
     if (!this.props.email) {
