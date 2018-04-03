48af79f00df9326e40080e85dfe107d3e816ec8f
quietmind <adammateo@gmail.com>
2018-04-03 19:44:24 -0400

    glogin styling

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -39,7 +39,7 @@ export default class Welcome extends React.Component{
         <a href='oauth'>
           <div id="gSignInWrapper">
             <div id="customBtn" class="customGPlusSignIn">
-              <span className="buttonText">Sign in with</span>
+              <span className="buttonText">Log in with:</span>
               <span className="icon"></span>
             </div>
           </div>
