b215cabdf91485a3db1600d86b75168b789ce8c6
Admin <admin@Admins-MacBook-Air.local>
2018-04-04 16:42:00 -0400

    Bug Squasher

--- client/src/components/DocList.jsx
+++ client/src/components/DocList.jsx
@@ -29,6 +29,7 @@ class DocList extends React.Component {
   }
 
   render() {
+    console.log(this.prepareFilesForGrid());
     const { pageNumber, numPages, scale } = this.state;
     if (this.props.fileList.length > 0) {
       return (
